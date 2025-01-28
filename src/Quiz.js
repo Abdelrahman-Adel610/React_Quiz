b77493eb8fa5ac2c40d7ac06b43059f89ef2f290
Abdelrahman Adel Hashiem <149705123+Abdelrahman-Adel610@users.noreply.github.com>
2025-01-28 23:36:19 +0200

    Timer is working

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,11 +1,14 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 
 const initialState = {
   score: 0,
   status: "notAnswered",
   selected: -1,
   crntQ: 0,
-  timer: "10:00",
+  timer: {
+    min: 10,
+    sec: 0,
+  },
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -23,21 +26,46 @@ function reducer(state, action) {
         status: "notAnswered",
         crntQ: state.crntQ + 1,
       };
+    case "decTime":
+      if (state.timer.sec === 0 && state.timer.min > 0) {
+        return {
+          ...state,
+          timer: {
+            sec: 59,
+            min: state.timer.min - 1,
+          },
+        };
+      } else if (state.timer.sec > 0)
+        return {
+          ...state,
+          timer: {
+            sec: state.timer.sec - 1,
+            min: state.timer.min,
+          },
+        };
+      else return { ...state };
     default:
       throw new Error("Undefined action");
   }
 }
 export default function Quiz({ Questions }) {
-  const [{ status, score, selected, timer, crntQ }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-  const q = {
-    question: "Which is the most popular JavaScript framework?",
-    options: ["Angular", "React", "Svelte", "Vue"],
-    correctOption: 1,
-    points: 10,
-  };
+  const [
+    {
+      status,
+      score,
+      selected,
+      timer: { sec, min },
+      crntQ,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+  useEffect(function () {
+    function callBack() {
+      dispatch({ type: "decTime" });
+    }
+    setInterval(callBack, 1000);
+    return clearInterval(callBack);
+  }, []);
   const { question, options, correctOption, points } = Questions[crntQ];
   return (
     <div>
@@ -79,7 +107,9 @@ export default function Quiz({ Questions }) {
           </button>
         ))}
       </div>
-      <div className="timer">{timer}</div>
+      <div className="timer">
+        {min.toString().padStart(2, 0)}:{sec.toString().padStart(2, 0)}
+      </div>
       <button
         className={`btn btn-ui ${selected === -1 ? "hidden" : ""}`}
         onClick={() => dispatch({ type: "nextQuestion" })}
